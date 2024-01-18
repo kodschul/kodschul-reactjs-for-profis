5eac53f6b21b1cc49b3c533c44950fc4ba4bb7d5
franznkemaka <franz@rigle.co>
2024-01-18 15:36:50 +0100

    ✨ redux-persist

--- code/react-app/src/redux/App.js
+++ code/react-app/src/redux/App.js
@@ -4,20 +4,23 @@ import { useEffect, useRef, useState, useReducer } from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import Counter from "./Counter";
 import Watcher from "./Watcher";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./store";
+import store, { persistor } from "./store";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <Watcher />
-          <img src={logo} className="App-logo" alt="logo" />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <div className="App">
+          <header className="App-header">
+            <Watcher />
+            <img src={logo} className="App-logo" alt="logo" />
 
-          <Counter />
-        </header>
-      </div>
+            <Counter />
+          </header>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
