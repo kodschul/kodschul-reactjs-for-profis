61169315bc681dde3a96658a9f8452dc0de85b55
franznkemaka <franz@rigle.co>
2024-01-18 15:18:47 +0100

    ✨ zustand

--- code/react-app/src/index.js
+++ code/react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./redux/App";
+import App from "./zustand/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
