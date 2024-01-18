72dab1c5fb09b3e1dc92451b26ea2a549c275858
franznkemaka <franz@rigle.co>
2024-01-18 09:19:47 +0100

    ✨

--- code/react-app/src/index.js
+++ code/react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./memo";
+import App from "./use-context";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- code/react-app/src/use-context.js
+++ code/react-app/src/use-context.js
@@ -3,6 +3,35 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { ThemeConsumer, ThemeProvider } from "./theme";
 
+const withTheme = (Component) => {
+  return (props) => {
+    return (
+      <ThemeConsumer>
+        {({ theme, setTheme }) => (
+          <Component {...props} theme={theme} setTheme={setTheme} />
+        )}
+      </ThemeConsumer>
+    );
+  };
+};
+
+function MyAppDummy({ theme, setTheme }) {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            Theme mode is: {theme}
+          </div>
+          {/* <ListComponent /> */}
+        </header>
+      </div>
+    </>
+  );
+}
+
 function MyApp() {
   return (
     <>
@@ -26,11 +55,13 @@ function MyApp() {
   );
 }
 
+const ThemedApp = withTheme(MyAppDummy);
+
 const AppContainer = () => {
   const [theme, setTheme] = useState("dark");
   return (
     <ThemeProvider value={{ theme, setTheme }}>
-      <MyApp />
+      <ThemedApp />
     </ThemeProvider>
   );
 };
