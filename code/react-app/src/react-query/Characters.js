a5154d5bce8323afbd3dd9b04e358887e5ab9088
franznkemaka <franz@rigle.co>
2024-01-19 09:32:33 +0100

    ✨ react-query

---
+++ code/react-app/src/react-query/Characters.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchCharacters = async (page) => {
+  const res = await axios.get("https://rickandmortyapi.com/api/character", {
+    params: { page },
+  });
+  return res?.data?.results;
+};
+
+const Characters = () => {
+  const [page, setPage] = useState(1);
+  const { data: characters } = useQuery(
+    ["characters", page],
+    () => fetchCharacters(page),
+    {
+      retry: 3,
+    }
+  );
+
+  return (
+    <div>
+      Characters
+      {characters?.map?.((character) => (
+        <div key={character.id} style={{ margin: 20 }}>
+          <img src={character.image} width={100} />
+          <div>{character.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Characters;
