47c2d0298af6e8e0ee0511b87488b6504a25d7b5
franznkemaka <franz@rigle.co>
2024-01-19 11:04:21 +0100

    ✨ swr

--- code/react-app/src/index.js
+++ code/react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./react-query/App";
+import App from "./swr/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
